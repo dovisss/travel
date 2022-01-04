8893c97e16590183413217f3bfa9b3a26050c1cd
1326241 <978752208@qq.com>
2022-01-04 22:07:39 +0800

    7-6图标icons实现

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,100 +1,114 @@
-<!--/* eslint-disable */-->
-<!--<template>-->
-<!--  <div class="icons">-->
-<!--    <swiper>-->
-<!--      <swiper-slide>-->
-<!--        <div class="icon" v-for="item of iconList" key="item.id">-->
-<!--          <div class="icon-img">-->
-<!--            <img class="icon-img-content" src="item.imgUrl"/>-->
-<!--          </div>-->
-<!--          <p class="desc">热门景点</p>-->
-<!--        </div>-->
-<!--      </swiper-slide>-->
-<!--    </swiper>-->
-<!--    <div>-->
+/* eslint-disable */
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl"/>
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
 
-<!--    </div>-->
-<!--  </div>-->
-<!--</template>-->
+<script>
 
-<!--<script>-->
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+        desc: "滑雪季"
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+        desc: '泡温泉'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+        desc: '动植园'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png',
+        desc: '游乐园'
+      }, {
+        id: "0006",
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png',
+        desc: '必游榜单'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/17/4bd370f3eb1acd02.png',
+        desc: '演出'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png',
+        desc: '城市观光'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }],
+    }
+  },
+  computed: {
+    pages () { // pages这个数据是由函数返回出来的内容
+      const pages = []
+      this.iconList.forEach((item, index) => { // 循环项和循环项对应的下标
+        const page = Math.floor(index / 8) // 页码
+        if (!pages[page]) { // 如果不存在
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      console.log(pages)
+      return pages;
+    }
+  }
+}
+</script>
 
-<!--export default {-->
-<!--  name: 'HomeIcons',-->
-<!--  data () {-->
-<!--    return {-->
-<!--      "iconList": [{-->
-<!--        id: '0001',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',-->
-<!--        desc: '景点门票'-->
-<!--      }, {-->
-<!--        id: '0002',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',-->
-<!--        desc: "滑雪季"-->
-<!--      }, {-->
-<!--        id: '0003',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',-->
-<!--        desc: '泡温泉'-->
-<!--      }, {-->
-<!--        id: '0004',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',-->
-<!--        desc: '动植园'-->
-<!--      }, {-->
-<!--        id: '0005',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png',-->
-<!--        desc: '游乐园'-->
-<!--      }, {-->
-<!--        id: "0006",-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png',-->
-<!--        desc: '必游榜单'-->
-<!--      }, {-->
-<!--        id: '0007',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/17/4bd370f3eb1acd02.png',-->
-<!--        desc: '演出'-->
-<!--      }, {-->
-<!--        id: '0008',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png',-->
-<!--        desc: '城市观光'-->
-<!--      }, {-->
-<!--        id: '0009',-->
-<!--        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',-->
-<!--        desc: '一日游'-->
-<!--      }],-->
-<!--    }-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
-
-<!--<style lang="stylus" scoped>-->
-<!--  @import '~styles/varibles.styl';-->
-<!--  .icons >>> .swiper-container-->
-<!--    height: 0-->
-<!--    padding-bottom: 50%-->
-<!--  .icon-->
-<!--    position: relative-->
-<!--    overflow: hidden-->
-<!--    float: left-->
-<!--    width: 25%-->
-<!--    height: 0-->
-<!--    padding-bottom: 25%-->
-<!--    .icon-img-->
-<!--      position: absolute-->
-<!--      top: 0-->
-<!--      left: 0-->
-<!--      right: 0-->
-<!--      bottom: .44rem-->
-<!--      box-sizing: border-box-->
-<!--      padding: .1rem-->
-<!--      .icon-img-content-->
-<!--        display: block-->
-<!--        margin: 0 auto-->
-<!--        height: 100%-->
-<!--    .icon-desc-->
-<!--      position: absolute-->
-<!--      text-align: center-->
-<!--      left: 0-->
-<!--      right: 0-->
-<!--      bottom: 0-->
-<!--      height: .44rem-->
-<!--      line-height: .44rem-->
-<!--</style>-->
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import "~styles/mixins.styl"
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      text-align: center
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      color: $darkTextColor
+      ellipsis()
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../pages/home/Home' // '@/pages/home/Home'
 import List from '../pages/list/List'
-import aaa from  '../pages/demo/aaa'
-import ab from '../pages/demo/ab'
-import Demo from '../pages/demo/demo'
+
 Vue.use(Router)
 // 当访问根路径时，
 export default new Router({
@@ -18,15 +16,6 @@ export default new Router({
       path: '/list',
       name: 'List',
       component: List
-    },
-    {
-      path: '/ab',
-      name: 'ab',
-      component: ab
-    },{
-      path: '/aaa',
-      name: 'aaa',
-      component: aaa
     }
   ]
 })
