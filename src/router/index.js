8f668967a3d575608bba948b99dd63657dffd309
dovisss <978752208@qq.com>
2022-01-15 21:27:07 +0800

    8.1 city router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../pages/home/Home' // '@/pages/home/Home'
-import List from '../pages/list/List'
+import City from '../pages/city/City'
 
 Vue.use(Router)
 // 当访问根路径时，
@@ -13,11 +13,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/list',
-      name: 'List',
-      component: List
+      path: '/city',
+      name: 'City',
+      component: City
     }
   ]
 })
-
-
