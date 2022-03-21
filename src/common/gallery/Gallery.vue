0501319a7c28a75e4cd4a76f7029239765f2fa95
dovisss <978752208@qq.com>
2022-03-21 18:29:27 +0800

    9.2画廊组建编码

--- src/common/gallery/Gallery.vue
+++ src/common/gallery/Gallery.vue
@@ -1,13 +1,66 @@
 <template>
-  <div>hhhhhhh</div>
+  <div class="container" @click="handleGalleryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class="gallery-img" :src="item"/>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'CommonGallery'
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true, // 一开始gallery处于隐藏状态，再次显示时swiper计算宽度有问题导致轮播图无法正常滚动
+        observe: true // 只要监听到该元素或父级元素dom结构变化，会自动刷新
+      }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('close')
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+  overflow inherit
+.container
+  display flex
+  flex-direction column
+  justify-content center
+  z-index 99
+  position fixed
+  left 0
+  right 0
+  top 0
+  bottom 0
+  background #000
+  .wrapper
+    height 0
+    width 100%
+    padding-bottom 100%
+    .gallery-img
+      width 100%
+    .swiper-pagination
+      color #fff
+      bottom -1rem
 </style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="banner">
+    <div class="banner" @click="handleBannerClick">
       <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"/>
       <div class="banner-info">
         <div class="banner-tittle">
@@ -12,7 +12,7 @@
         </div>
       </div>
     </div>
-    <common-gallery></common-gallery>
+    <common-gallery :imgs="imgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
   </div>
 </template>
 
@@ -22,6 +22,20 @@ export default {
   name: 'DetailBanner',
   components: {
     CommonGallery
+  },
+  data () {
+    return {
+      showGallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg', 'http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallery = true
+    },
+    handleGalleryClose () {
+      this.showGallery = false
+    }
   }
 }
 </script>
